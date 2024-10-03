e5493452c6af9794dac630ce3c5f7e0026483591
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2024-10-03 09:27:06 +0200

    Bankomat_done

--- src/while_A1.java
+++ src/while_A1.java
@@ -10,8 +10,9 @@ public class while_A1 {
         while (!isFinished){
             Random random = new Random();
 
-            int randomNumber1 = random.nextInt(16) + 15;
+            int randomNumber1 = random.nextInt(21) + 10;
             summe += randomNumber1;
+            System.out.println(randomNumber1);
 
             if (randomNumber1 == 15 || randomNumber1 == 25){
                 System.out.println(summe);
