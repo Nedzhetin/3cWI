88e358631c3d555b17070712d038a428b6c36645
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2024-10-12 11:42:56 +0200

    better_Folder_Structure

--- src/loops_A1.java
+++
@@ -1,9 +0,0 @@
-public class loops_A1 {
-    public static void main(String[] args) {
-        int summe = 0;
-        for (int i = 0; i <= 100; i++) {
-             summe = summe + i;
-        }
-        System.out.println(summe);
-    }
-}
